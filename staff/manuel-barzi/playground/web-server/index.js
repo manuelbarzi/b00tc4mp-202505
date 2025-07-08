11172e03ed57aeef888268ecc11eb639401e3af1
manuelbarzi <manuelbarzi@gmail.com>
2025-07-08 14:15:08 +0200

    implement search with filter; implement add to cart, and cart view (with back button and total calculation); improve cameras data with more context info for next functionalities; #1

--- staff/manuel-barzi/playground/web-server/index.js
+++ staff/manuel-barzi/playground/web-server/index.js
@@ -3,6 +3,9 @@ const { data } = require('./data/index.js')
 
 const server = express()
 
+let query
+const cart = []
+
 server.get('/', (request, response) => {
     response.send('Hello, World!')
 })
@@ -10,11 +13,75 @@ server.get('/', (request, response) => {
 server.get('/search', (request, response) => {
     const { q } = request.query
 
+    query = q
+
+    let cameras = data.loadCameras()
+
+    cameras = cameras.filter(camera => camera.brand.toUpperCase() === q.toUpperCase())
+
+    response.send(`<doctype html>
+        <html>
+            <head>
+                <title>Results</title>
+            </head>
+
+            <body>
+                <h2>Results</h2>
+                <a href="http://localhost:8080/cart">Cart</a>
+                <ul>
+                    ${cameras.map(({ id, brand, model, type, filmFormat, price }) => `<li>
+                            <h3>${brand} ${model}</h3>
+
+                            <i>${type} ${filmFormat}</i>
+
+                            <strong>${price}</strong>
+
+                            <a href="http://localhost:8080/products/${id}/add">Add</a>
+
+                            <a href="https://${brand}.com">${brand}</a>
+                        </li>`)}
+                </ul>
+            </body>
+        </html>`)
+})
+
+server.get('/products/:id/add', (request, response) => {
+    const { id } = request.params
+
+    cart.push(id)
+
+    console.debug(cart)
+
+    response.redirect(`/search?q=${query}`)
+})
+
+server.get('/cart', (request, response) => {
     const cameras = data.loadCameras()
+    const items = cart.map(id => cameras.find(camera => camera.id === id))
 
-    // TODO filter cameras according to query (q)
+    response.send(`<doctype html>
+        <html>
+            <head>
+                <title>Cart</title>
+            </head>
 
-    response.send(cameras)
+            <body>
+                <h2>Cart</h2>
+                <a href="http://localhost:8080/search?q=${query}">Back</a>
+                <ul>
+                    ${items.map(({ id, brand, model, type, filmFormat, price }) => `<li>
+                            <h3>${brand} ${model}</h3>
+
+                            <i>${type} ${filmFormat}</i>
+
+                            <strong>${price}</strong>
+
+                            <a href="https://${brand}.com">${brand}</a>
+                        </li>`)}
+                </ul>
+                <strong>Total: ${items.reduce((acc, item) => acc + item.price, 0)}
+            </body>
+        </html>`)
 })
 
 server.listen(8080, () => console.log('Server is up on port 8080'))
